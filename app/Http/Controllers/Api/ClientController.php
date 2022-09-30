f3b700d773f992b24e06b22a713ab8c42b754191
David Simon Bolivar Castro <simon.bolivar@alarcongroup.us>
2022-09-30 10:06:04 -0500

    search ruc

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -10,6 +10,7 @@ use App\Models\KindPerson;
 use App\Models\Province;
 use Illuminate\Http\Request;
 use App\Http\Services\Eldni;
+use GuzzleHttp\Client;
 
 class ClientController extends Controller
 {
@@ -43,7 +44,45 @@ class ClientController extends Controller
     }
     public function getInfoFromDni(Request $request)
     {
-        $data = collect($this->dniService->get_name($request->nro_doc))->values();
-        return $data;
+        // $data = collect($this->dniService->get_name($request->nro_doc))->values();
+        $token = 'apis-token-2916.R0BrPlBD2-xz2HNYFQ1O4SxbwWJ2zqxP';
+        $client = new Client(['base_uri' => 'https://api.apis.net.pe', 'verify' => false]);
+        $parameters = [
+            'http_errors' => false,
+            'connect_timeout' => 5,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Referer' => 'https://apis.net.pe/api-consulta-dni',
+                'User-Agent' => 'laravel/guzzle',
+                'Accept' => 'application/json',
+            ],
+            'query' => ['numero' => $request->nro_doc]
+        ];
+        // return $parameters;
+        $res = $client->request('GET', '/v1/dni', $parameters);
+        $resp = json_decode($res->getBody()->getContents(), true);
+        return response()->json($resp, 200);
+    }
+    public function getInfoRuc(Request $request)
+    {
+        $token = 'apis-token-2916.R0BrPlBD2-xz2HNYFQ1O4SxbwWJ2zqxP';
+
+        $client = new Client(['base_uri' => 'https://api.apis.net.pe', 'verify' => false]);
+
+        $parameters = [
+            'http_errors' => false,
+            'connect_timeout' => 5,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Referer' => 'https://apis.net.pe/api-consulta-ruc',
+                'User-Agent' => 'laravel/guzzle',
+                'Accept' => 'application/json',
+            ],
+            'query' => ['numero' => $request->nro_ruc]
+        ];
+        $res = $client->request('GET', '/v1/ruc', $parameters);
+        $resp = json_decode($res->getBody()->getContents(), true);
+
+        return response()->json($resp, 200);
     }
 }
